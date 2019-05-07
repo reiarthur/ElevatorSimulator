abb54832e9a80ffc62b0e881831944796f88f1e2
reiarthur <aomm@cin.ufpe.br>
2019-05-07 12:54:23 -0300

    Employee's names because representation matters 👏

--- SolvingQuestion2/SolvingQuestion2.cs
+++ SolvingQuestion2/SolvingQuestion2.cs
@@ -10,8 +10,8 @@ namespace SolvingQuestion2
     {
         public static void Run()
         {
-            Person p1 = new Person(8, 0, "Leandro", true);
-            Person p2 = new Person(0, 7, "Cláusio", true);
+            Person p1 = new Person(8, 0, "Welington", true);
+            Person p2 = new Person(0, 7, "Camila", true);
             Person p3 = new Person(3, 15, "Alana", true);
 
             Elevator elevator = new Elevator(20, 3, false, true, p1, p2, p3);
--- SolvingQuestion3/Program.cs
+++ SolvingQuestion3/Program.cs
@@ -8,16 +8,16 @@ namespace SolvingQuestion3
         static void Main(string[] args)
         {
             //Question1
-            Person p1 = new Person(8, 0, "Leandro", false);
-            Person p2 = new Person(0, 10, "Cláusio", false);
+            Person p1 = new Person(8, 0, "Silvano", false);
+            Person p2 = new Person(0, 10, "Marcela", false);
             Console.WriteLine("================= Question 1 =================");
             SolvingQuestion3.Run(10, 5, p1, p2);
 
 
             //Question2
-            Person p3 = new Person(8, 0, "Leandro", false);
-            Person p4 = new Person(0, 7, "Cláusio", false);
-            Person p5 = new Person(3, 15, "Alana", false);
+            Person p3 = new Person(8, 0, "Renato", false);
+            Person p4 = new Person(0, 7, "Geisy", false);
+            Person p5 = new Person(3, 15, "André", false);
             Console.WriteLine("================= Question 2 =================");
             SolvingQuestion3.Run(20, 3, p3, p4, p5);
 
